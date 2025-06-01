a690adea2e47e53797bc2a9fc28e3ebcb5b14089
Leezy <191919qaz@gmail.com>
2025-06-01 11:59:22 +0300

    taksopark almost complete

---
+++ B.taksopark/taksopark.h
@@ -0,0 +1,25 @@
+#ifndef TAKSOPARK_H_
+#define TAKSOPARK_H_
+
+// INCLUDES
+#include <stdio.h>
+
+// DEFINES
+#define MAX_CARS_AND_FRIENDS 200000
+#define MAX_PRICE 10000
+#define MAX_VACATION 100000000
+
+// STRUCTERS
+typedef struct Friends {
+    long long FirstVacationDay;
+    long long LastVacationDay;
+    int CarPrice;
+} Friend;
+
+//FUNCTIONS
+int ScanfFriend(Friend *person, int NumberOfFriends);
+void ProfitSort(Friend *person, int NumberOfFriends);
+int MaxDayToWork(Friend *person, int NumberOfFriends);
+int ProfitFunction(int NumberOfFriends, Friend *person, int NumberOfCars);
+
+#endif // TAKSOPARK_H_
